bf95eb833b24300a162125f73a57ddcd24dde6f4
gasupidupi <gasupidupi@gmail.com>
2023-04-28 23:26:12 +0200

    structure

---
+++ script.js
@@ -0,0 +1,82 @@
+$(document).ready(function(){
+
+  var isDropdownOpen = false
+  var previousMarginLeft = ""
+  $(".link").find($(".linkline")).css('visibility', 'hidden')
+
+  $(window).on( "resize", function() {
+  $( "#log" ).append( "<div>Handler for `resize` called.</div>" );
+    if($(window).width() <= 850) {
+      if(isDropdownOpen == false) {
+        $(".sidebar").css("margin-left", "-50%")
+      }
+      previousMarginLeft = "-50%"
+    } else {
+      if(isDropdownOpen == false) {
+        $(".sidebar").css("margin-left", "-11em")
+      }
+      previousMarginLeft = "-11em"
+    }
+  });
+
+  $(".link").hover(function(){
+    $(this).find($("a")).css("color", "#FF715B")
+    $(this).find($(".linkline")).css('visibility', 'visible')
+    $(this).find($(".linkline")).css("width", $(".link").find($("a")).css("width"))
+    $(this).find($(".linkline")).find($("rect")).css("width", $(".link").find($("a")).css("width"))
+  }, function () {
+    $(this).find($(".linkline")).css("width", 0)
+    $(this).find($(".linkline")).find($("rect")).css("width", 0)
+    $(this).find($(".linkline")).css('visibility', 'hidden')
+    $(this).find($("a")).css("color", "#2f52e0")
+  });
+
+  $(".link").click(function(){
+  }, function () {
+
+    if($(this).text().includes("Project")) {
+      $(".maincontent").load("project.html");;
+    }
+  });
+
+  $(".dropdown").click(function(){
+
+  }, function () {
+    if(isDropdownOpen == false) {
+      sidebarResetWidth = $(".sidebar").css("margin-left");
+      isDropdownOpen = true;
+      $(".line").css("color", "#F9CB40")
+      $("#line1").css("transform", "rotate(135deg)")
+      $("#line1").css("top", "45%")
+      $("#line3").css("transform", "rotate(-135deg)")
+      $("#line3").css("top", "45%")
+      $("#line2").hide()
+      previousMarginLeft = $(".sidebar").css("margin-left")
+      $(".sidebar").css("margin-left", "0em")
+    } else {
+      isDropdownOpen = false;
+      $(".line").css("color", "#F9CB40")
+      $("#line1").css("transform", "revert")
+      $("#line1").css("top", "25%")
+      $("#line3").css("transform", "revert")
+      $("#line3").css("top", "65%")
+      $("#line2").show()
+      $(".sidebar").css("margin-left", previousMarginLeft)
+    };
+  });
+
+  $(".dropdown").hover(function(){
+    if(isDropdownOpen == false) {
+      $(".line").css("color", "#bced09")
+    } else {
+      $(".line").css("color", "#ff715b")
+    }
+  }, function () {
+    if(isDropdownOpen == false) {
+      $(".line").css("color", "#F9CB40")
+    } else {
+      $(".line").css("color", "#F9CB40")
+    }
+  });
+
+});
